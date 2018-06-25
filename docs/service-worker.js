bb4527847200fac13120785351fb66c9b1cf1f94
Will in China <email.will.in.china@gmail.com>
2018-06-25 16:41:57 +0800

    version numbers

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","01d2e43432d78310d2454dc62a6877da"],["manifest.json","1845171d3c4fe9ebe6a48852f4347b34"],["src/blox-about.js","18717e34ccc6baa130fe564e959d7238"],["src/blox-app.js","67f1c0b2cb5674657df99dce20190290"],["src/blox-contact.js","bab1fca18d8b640aa4be2b962fb81309"],["src/blox-portfolio.js","7fb0164e30b10c757f8fca097fbc761b"],["src/blox-team.js","4034ab2c160dbb96e18048e7644f0ddb"],["src/components/blox-footer.js","62588fcad2837a5d774831cb08f3a082"],["src/components/blox-header.js","08497bbf9eb6ba7d95fd55d14b914d33"],["src/components/blox-spacer.js","436f4d83b620fce0832721525ce41df1"],["src/shared-styles.js","29a1cfaeb1feea315397213d6a2ba7dc"]];
+var precacheConfig = [["index.html","01d2e43432d78310d2454dc62a6877da"],["manifest.json","1845171d3c4fe9ebe6a48852f4347b34"],["src/blox-about.js","18717e34ccc6baa130fe564e959d7238"],["src/blox-app.js","67f1c0b2cb5674657df99dce20190290"],["src/blox-contact.js","bab1fca18d8b640aa4be2b962fb81309"],["src/blox-portfolio.js","2c7b44ccd4a82d5773660d283797f78f"],["src/blox-team.js","4034ab2c160dbb96e18048e7644f0ddb"],["src/components/blox-footer.js","62588fcad2837a5d774831cb08f3a082"],["src/components/blox-header.js","08497bbf9eb6ba7d95fd55d14b914d33"],["src/components/blox-spacer.js","436f4d83b620fce0832721525ce41df1"],["src/shared-styles.js","29a1cfaeb1feea315397213d6a2ba7dc"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
--- src/blox-portfolio.js
+++ src/blox-portfolio.js
@@ -63,6 +63,7 @@ class bloxPortfolio extends PolymerElement {
 
             <div class="component">
               <div class="title">Blox-Mnemonic</div>
+              <div class="version">v1.0.0</div>
               <p class="description">Generates a random or from password 12 word Mnemonic phrase </p>
               <a href="https://github.com/EOSBlox/blox-mnemonic" target="_blank"><img src="../images/github.svg"></a>
               <a href="https://www.npmjs.com/package/blox-mnemonic" target="_blank"><img src="../images/npm.svg"></a>
@@ -70,6 +71,7 @@ class bloxPortfolio extends PolymerElement {
 
             <div class="component">
               <div class="title">Blox-Scrypt</div>
+              <div class="version">v1.0.0</div>
               <p class="description">Generates a hash using the Scrypt KDF </p>
               <a href="https://github.com/EOSBlox/blox-scrypt" target="_blank"><img src="../images/github.svg"></a>
               <a href="https://www.npmjs.com/package/blox-scrypt" target="_blank"><img src="../images/npm.svg"></a>
