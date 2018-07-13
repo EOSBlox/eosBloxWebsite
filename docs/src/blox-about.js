93a1c59d130f742176752f3c3e1ed8214715360f
Will in China <email.will.in.china@gmail.com>
2018-07-13 12:27:28 +0800

    button text

--- docs/src/blox-about.js
+++ docs/src/blox-about.js
@@ -18,7 +18,7 @@ define(["./blox-app.js"],function(_bloxApp){"use strict";class BloxAbout extends
           <h3>EOS Blox is the worlds first suite of web components specifically for the creative developement of EOS DApps.</h3>
           <p>Web components are like building blocks, you make a few components that perform a single task and then join them together into a single new component, this approach significantly accelerate development times, shorterns the learning curve and stops duplicate code from being writen, of which the side effect is happy developers! </p>
           <p>We lovingly craft each reusable components to form the building blocks of your next EOS DApp. EOS Blox allow you to keep all the style and expression that makes your dapp unique to you, yet provide you with common components that you can re-use. Web components run nativly in the browser </p>
-          <a href="/portfolio"><button class="green-bg">show me a web component</button></a>
+          <a href="/portfolio"><button class="green-bg">show me the components</button></a>
           <blox-spacer space="90"></blox-spacer>
         </div>
       </div>
